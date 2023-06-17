df5c45be17c2b4c166927b60c94c3c76ce08c56c
Amit Ghade <ghadeamit@gmail.com>
2023-06-17 15:22:58 +0530

    made minor correcrtions in props name

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -3,9 +3,9 @@ import './ExpenseDate.css';
 
 
 function ExpenseDate(props) {
-  const month = props.datess.toLocaleString("en-US", { month: "long" });
-  const day = props.datess.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.datess.getFullYear();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className='expense-date'>
