92603f552ea2ed4e469b66c4220d6d349a3ff317
Amit Ghade <ghadeamit@gmail.com>
2023-06-17 14:25:11 +0530

    Adding Javascript logic to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props /* we can also write {date,title,amount} instead of props. This is known as object destructuring */) {
+  const month = props.date.toLocaleString('en-US', {month:'long'});
+  const day = props.date.toLocaleString('en-US', {day:'2-digit'});
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toString()/* is using object destructuring syntax we can write the code as date.toString()*/}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+        </div>  {/*if using object destructuring syntax we can write the code as date.toString()} */}
       <div className="expense-item__description">
-        <h2>{props.title /* is using object destructuring syntax we can write the code as title only*/}</h2>
-        <div className="expense-item__price">${props.amount /* is using object destructuring syntax we can write the code as amount only*/}</div>
+        <h2>{props.title} </h2> {/* is using object destructuring syntax we can write the code as title only */}
+        <div className="expense-item__price">${props.amount }</div> {/* is using object destructuring syntax we can write the code as amount only*/}
       </div>
     </div>
   );
