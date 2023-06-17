5961c6822b2c9c43c8ba68c4bf02007325e83c0b
Amit Ghade <ghadeamit@gmail.com>
2023-06-17 12:24:30 +0530

    Added comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-function App(props) {
+function App() {
   const expenses = [
     {
       id: "e1",
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+function ExpenseItem(props /* we can also write {date,title,amount} instead of props. This is known as object destructuring */) {
   return (
     <div className="expense-item">
-      <div>{props.date.toString()}</div>
+      <div>{props.date.toString()/* is using object destructuring syntax we can write the code as date.toString()*/}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{props.title /* is using object destructuring syntax we can write the code as title only*/}</h2>
+        <div className="expense-item__price">${props.amount /* is using object destructuring syntax we can write the code as amount only*/}</div>
       </div>
     </div>
   );
